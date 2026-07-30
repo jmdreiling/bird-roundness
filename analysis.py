bdd2b26c016db9afb031bda9f1333e923b5123ba
julia <julia.dreiling@outlook.com>
2026-07-30 14:10:57 +0100

    all of daylight?

--- analysis.py
+++ analysis.py
@@ -7,7 +7,7 @@ with open('eccentricity_dict.json', 'r') as source:
 
 values_list = [(key,val) for key, val in eccentricity_dict.items() if val != 'none found']
 sorted_values_list = sorted(values_list, key=lambda item: item[1])
-roundest_bird_pair = sorted_values_list[4]
+roundest_bird_pair = sorted_values_list[0]
 roundest_bird_path= f"./{roundest_bird_pair[0]}"
 print(roundest_bird_path)
 print(roundest_bird_pair)
